f231670ba04e061eb9df83fbd8455c84f1396879
yves848 <yves.godart@gmail.com>
2022-02-11 18:07:29 +0100

    observables OK

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,17 +1,45 @@
 import { Injectable } from '@angular/core';
 import { fileList } from '../../../src-backend/interfaces/interface'
+import { IpcService } from './ipc.service';
+import { Observable, BehaviorSubject } from 'rxjs';
 
+const electron = (<any>window).require('electron');
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
   path = '';
+  //$files: Observable<fileList[]>;
   files: fileList[] = [];
+  readonly EMPTY_STATE: fileList[] = [];
+
+
   selectedFile: fileList = {
     file: "",
     active: false,
     tags: undefined
   }
 
-  constructor() { }
+  constructor(private ipc: IpcService, private data: DataService) {
+  }
+
+  loadFiles(path: string): any {
+    console.log('loadFiles', path);
+    const filesObservable = new Observable<fileList[]>(observer => {
+      this.getFiles2(path).then((files) => {
+        if (files) {
+          observer.next(files);
+        } else {
+          observer.next(this.EMPTY_STATE);
+        }
+      })
+    })
+    return filesObservable;
+
+  }
+
+  getFiles2(dir: string): Promise<fileList[]> {
+    return electron.ipcRenderer.invoke('getFiles_Promise', dir);
+
+  }
 }
